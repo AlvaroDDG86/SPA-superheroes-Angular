90d24d7d5b50e4f32f3915fc6d8c22d93bca6f3d
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2017-11-19 18:45:33 +0100

    Buscar al presionar enter, y detalle cuando buscas

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import  {HeroesService} from '../../services/heroes.service'
 
 @Component({
@@ -10,13 +11,19 @@ import  {HeroesService} from '../../services/heroes.service'
 export class BuscadorComponent implements OnInit {
   heroes:any[] = [];
   termino:string;
-  constructor(private _activatedRoute:ActivatedRoute, private  _heroesService:HeroesService) { }
+  constructor(private _activatedRoute:ActivatedRoute,
+              private router:Router,
+              private  _heroesService:HeroesService) { }
 
   ngOnInit() {
     this._activatedRoute.params.subscribe(params =>{
       this.termino = params['termino'];
       this.heroes = this._heroesService.buscarHeroes(params['termino']);
     })
+  }
+
+  verHeroe(index:number){
+    this.router.navigate(['/heroe', index]);
   }
 
 }
